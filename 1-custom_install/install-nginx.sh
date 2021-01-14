f7422015308397549b1fdce886247f3b306201f8
Lalit R Nagpal <lalit2801@gmail.com>
2021-01-14 17:57:09 +0530

    Update install-nginx.sh

--- 1-custom_install/install-nginx.sh
+++ 1-custom_install/install-nginx.sh
@@ -24,6 +24,8 @@ sudo make install
 
 sudo openssl req -x509 -days 10 -nodes -newkey rsa:2048 -keyout /etc/nginx/ssl/self.key -out /etc/nginx/ssl/self.crt
 
+sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
+
 ls -l /etc/nginx
 
 nginx -V
