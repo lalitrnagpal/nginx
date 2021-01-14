d416f846b8e142a6592fedd0939191e0f65d3796
Lalit R Nagpal <lalit2801@gmail.com>
2021-01-14 17:49:29 +0530

    Update install-nginx.sh

--- 1-custom_install/install-nginx.sh
+++ 1-custom_install/install-nginx.sh
@@ -18,6 +18,8 @@ sudo mkdir /etc/nginx/ssl
 
 sudo openssl req -x509 -days 10 -nodes -newkey rsa:2048 -keyout /etc/nginx/ss
 
+sudo openssl req -x509 -days 10 -nodes -newkey rsa:2048 -keyout /etc/nginx/ssl/self.key -out /etc/nginx/ssl/self.crt
+
 ./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-pcre --pid-path=/var/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --with-http_v2_module
 
 sudo make
