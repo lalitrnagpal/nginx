47f2b204b616519ae4e1be9592d8ca94d3810f86
Lalit Nagpal <lalit2801@gmail.com>
2021-01-15 13:02:41 +0530

    added header to response

--- 19-reverse-proxy-load-balancing/myserver/src/main/java/com/example/webserver/myserver/controllers/MyController.java
+++ 19-reverse-proxy-load-balancing/myserver/src/main/java/com/example/webserver/myserver/controllers/MyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 	
 	@GetMapping("/hello")
-	public String sayHello(HttpServletRequest request) {
+	public String sayHello(HttpServletRequest request, HttpServletResponse response) {
 		
 		String requestedUrl = request.getRequestURI(); 
 		StringBuffer headerNames = new StringBuffer("");
 		request.getHeaderNames().asIterator().forEachRemaining( name -> headerNames.append(name + ",") );
 		List<String> headers = Arrays.asList(headerNames.toString().split(",")).stream().filter(name -> name.trim().length() > 0 ).map( name -> name + ": " + request.getHeader(name) ).collect( Collectors.toList() );
+		
+		response.setHeader("myheader", "myvalue");
 		return "Hello from Spring Boot! You requested for " + headers.toString();
 	}
 
